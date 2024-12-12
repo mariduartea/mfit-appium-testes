1f1e47f65592254d4c11610a2456b7dabc825292
Mariana Reboucas <marianarda14@gmail.com>
2024-12-12 16:57:49 -0300

    adicionando arquivo de setup

---
+++ setup.py
@@ -0,0 +1,52 @@
+from appium import webdriver
+from appium.options.common.base import AppiumOptions
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.wait import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+from config import locators,test_data
+
+
+class SetUp:
+    @classmethod
+
+    def setUpClass(cls):
+        options = AppiumOptions()
+        options.load_capabilities({
+            "platformName": "Android",
+            "appium:automationName": "UiAutomator2",
+            "appium:ensureWebviewsHavePages": True
+        })
+
+        cls.driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+        cls.pacote = "app.mfit.personal"
+        cls.driver.activate_app(cls.pacote)
+
+        #login
+        cls.login(cls.driver)
+
+    @classmethod
+    def login(cls, driver):
+
+        #fazer login
+        personal_opt = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, locators["personal_opt"])))
+        personal_opt.click()
+
+        email_personal = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, locators["email_personal"])))
+        email_personal.send_keys(test_data["email_personal"])
+
+        senha_personal = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, locators["senha_personal"])))
+        senha_personal.send_keys(test_data["senha_personal"])
+
+        btn_login = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, locators["btn_login"])))
+        btn_login.click()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
